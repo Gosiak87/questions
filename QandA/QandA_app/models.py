b735b76a1c6ef7656177bf51ddd2fb200d1959c7
Gosia <miziolekgosia@gmail.com>
2018-09-25 15:17:53 +0200

    add technology

--- QandA/QandA_app/models.py
+++ QandA/QandA_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    TECHNOLOGY = (
+        (-1, "undefined"),
+        (1, "Python"),
+        (2, "Java Script"),
+        (3, "Linux"),
+        (4, "HTML"),
+        (5, "CSS")
+    )
+    content = models.CharField(max_length=300)
+    first_name = models.CharField(max_length=64)
+    answer = models.CharField(max_length=1000, null=True, blank=True)
+    technology = models.IntegerField(choices=TECHNOLOGY, deafault=-1)
